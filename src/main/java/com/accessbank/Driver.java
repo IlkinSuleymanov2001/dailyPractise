ccc1b9227a7feb7fdf59b8b37161b3b0d266f0b0
Cavid <cavidsuleymanov300@gmail.com>
2024-11-14 10:39:32 +0400

    used lombok annotation over  Person class and test

--- src/main/java/com/accessbank/Driver.java
+++ src/main/java/com/accessbank/Driver.java
@@ -1,25 +1,25 @@
 package com.accessbank;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.OptionalInt;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public class Driver {
 
     public static void main(String[] args) {
 
 
-        List<String >ss = Arrays.asList("salam","xxxsalam","goods","do you have how many year experience with  java ? ");
+        /*List<String >ss = Arrays.asList("salam","xxxsalam","goods","do you have how many year experience with  java ? ");
         String s = "sala";
         find2(s,ss).ifPresent(System.out::println);
 
         Optional<Integer> reduce = ss.stream().map(String::length)
-                .reduce((a, b) -> a + b);
+                .reduce((a, b) -> a + b);*/
 
-
+       /* Person p= Person.builder().FindCode("salam")
+                .age(23).name("ilkin").build();
+        System.out.println(p.toString());*/
 
 
     }
